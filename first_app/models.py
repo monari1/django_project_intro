93c07ed32d704b3b44a535277cf19b242acbacb2
monari1 <francisnyasinga@gmail.com>
2023-05-19 02:12:27 +0300

    learnt on how to activate models

--- first_app/models.py
+++ first_app/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 """ A topic the user is learning about """
-text = models.CharField(max_length= 200)
-date_added = models.DateTimeField(auto_now_add=True )
-
-def __str__(self):
-    """ Return  a string representation of the model """
-    return self.text
+class Topic(models.Model):
+    text = models.CharField(max_length= 200)
+    date_added = models.DateTimeField(auto_now_add=True )
+    def __str__(self):
+        """ Return  a string representation of the model """
+        return self.text
